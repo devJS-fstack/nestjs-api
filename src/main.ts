aedabede2b0c036881d831b0021c3efcfae1bde6
Tinh Nguyen <tinh.nguyen@pascal.studio>
2022-07-25 15:43:19 +0700

    config cors server

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,POST,PUT,PATCH'
+  })
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
